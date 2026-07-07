007da521ac4b54df789776a5912af0ec6230ed14
Ngquanghuy <ngquanghuy012@gmail.com>
2026-07-07 14:41:09 +0700

    chore: remove build_asan/ from tracking

--- build_asan/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ngquanghuy/Switch
-# Build directory: /home/ngquanghuy/Switch/build_asan
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
